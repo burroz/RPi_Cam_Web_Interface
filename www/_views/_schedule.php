a83a3a13dedbdfaff02c3e2c05bf11079c342490
burroz <git@burroz.com>
2015-05-21 21:31:37 +0200

    ...

--- www/_views/_schedule.php
+++ www/_views/_schedule.php
@@ -1,67 +1,31 @@
 <?php
-   #define('BASE_DIR', dirname(__FILE__));
-   #require_once(BASE_DIR.'/config.php');
-
-   
-   $debugString = "";
-   $schedulePars = array();
-   $schedulePars = loadPars(BASE_DIR . '/' . SCHEDULE_CONFIG);
-   
-   $cliCall = isCli();
-   $showLog = false;
-   $schedulePID = getSchedulePID();
-   if (!$cliCall && isset($_POST['action'])) {
-   //Process any POST data
-      switch($_POST['action']) {
-         case 'start':
-            startSchedule();
-            $schedulePID = getSchedulePID();
-            break;
-         case 'stop':
-            stopSchedule($schedulePID);
-            $schedulePID = getSchedulePID();
-            break;
-         case 'save':
-            writeLog('Saved schedule settings');
-            $fp = fopen(BASE_DIR . '/' . SCHEDULE_CONFIG, 'w');
-            $saveData = $_POST;
-            unset($saveData['action']);
-            fwrite($fp, json_encode($saveData));
-            fclose($fp);
-            $schedulePars = loadPars(BASE_DIR . '/' . SCHEDULE_CONFIG);
-            sendReset();
-            break;
-         case 'backup':
-            writeLog('Backed up schedule settings');
-            $fp = fopen(BASE_DIR . '/' . SCHEDULE_CONFIGBACKUP, 'w');
-            fwrite($fp, json_encode($schedulePars));
-            fclose($fp);
-            break;
-         case 'restore':
-            writeLog('Restored up schedule settings');
-            $schedulePars = loadPars(BASE_DIR . '/' . SCHEDULE_CONFIGBACKUP);
-            break;
-         case 'showlog':
-            $showLog = true;
-            break;
-         case 'downloadlog':
-            if (file_exists(BASE_DIR . '/' . LOGFILE_SCHEDULE)) {
-               header("Content-Type: text/plain");
-               header("Content-Disposition: attachment; filename=\"" . date('Ymd-His-') . LOGFILE_SCHEDULE . "\"");
-               readfile(BASE_DIR . '/' . LOGFILE_SCHEDULE);
-               return;
-            }
-         case 'clearlog':
-            if (file_exists(BASE_DIR . '/' . LOGFILE_SCHEDULE)) {
-               unlink(BASE_DIR . '/' . LOGFILE_SCHEDULE);
-            }
-            break;
-      }
-   }
-   
-   if (!$cliCall) {
-      mainHTML();
-   } else {
-      mainCLI();
-   }
+	if (!$cliCall) {
+		echo '<div class="container-fluid">';
+		echo '<form action="schedule.php" method="POST">';
+		if ($debugString) echo $debugString . "<br>";
+		if ($showLog) {
+			echo "&nbsp&nbsp;<button class='btn btn-primary' type='submit' name='action' value='downloadlog'>" . BTN_DOWNLOADLOG . "</button>";
+			echo "&nbsp&nbsp;<button class='btn btn-primary' type='submit' name='action' value='clearlog'>" . BTN_CLEARLOG . "</button><br><br>";
+			displayLog();
+		} else {
+			echo '<div class="container-fluid text-center">';
+			echo "&nbsp;&nbsp;<button class='btn btn-primary' type='submit' name='action' value='save'>" . BTN_SAVE . "</button>";
+			echo "&nbsp;&nbsp;<button class='btn btn-primary' type='submit' name='action' value='backup'>" . BTN_BACKUP . "</button>";
+			echo "&nbsp;&nbsp;<button class='btn btn-primary' type='submit' name='action' value='restore'>" . BTN_RESTORE . "</button>";
+			echo "&nbsp;&nbsp;<button class='btn btn-primary' type='submit' name='action' value='showlog'>" . BTN_SHOWLOG . "</button>";
+			echo '&nbsp;&nbsp;&nbsp;&nbsp;';
+			if ($schedulePID != 0) {
+				echo "<button class='btn btn-danger' type='submit' name='action' value='stop'>" . BTN_STOP . "</button>";
+			} else {
+				echo "<button class='btn btn-danger' type='submit' name='action' value='start'>" . BTN_START . "</button>";
+			}
+			echo "<br></div>";
+			showScheduleSettings($schedulePars);
+		}
+		echo '</form>';
+		echo '</div>';
+		cmdHelp();
+	} else {
+		mainCLI();
+	}
 ?>
