8f7cb39f8376ee876f3711fd305a56ee0aa6b018
burroz <git@burroz.com>
2015-05-22 21:38:47 +0200

    ...

--- www/cmd_pipe.php
+++ www/cmd_pipe.php
@@ -1,12 +1,9 @@
-aaa
 <?php
-   
-exit();
-	define('BASE_DIR', dirname(__FILE__));
-   require_once(BASE_DIR.'/config.php');
-
-   
-   $pipe = fopen("FIFO","w");
-   fwrite($pipe, $_GET["cmd"]);
-   fclose($pipe);
+	#define('BASE_DIR', dirname(__FILE__));
+	#include BASE_DIR . '/_includes/_applicationhelper.php';
+	#define('BASE_DIR', dirname(__FILE__));
+	#require_once(BASE_DIR.'/config.php');
+	$pipe = fopen("FIFO","w");
+	fwrite($pipe, $_GET["cmd"]);
+	fclose($pipe);
 ?>
