1384fa2fc6f616c1ffd778f15321429f77989a4e
burroz <git@burroz.com>
2015-05-23 13:09:32 +0200

    ...

--- www/_partials/_navi.php
+++ www/_partials/_navi.php
@@ -4,12 +4,14 @@
 		define('NAV_POS','index');
 	}
 	$systemButtons = '';
-	$systemButtons .= '<div>';
-	$systemButtons .= '<input id="toggle_stream" type="button" class="btn btn-default btn-block" value="' . $streamButton . '" onclick="set_stream_mode(this.value);">';
-	$systemButtons .= '<input id="shutdown_button" type="button" value="shutdown system" onclick="sys_shutdown();" class="btn btn-danger btn-block">';
-	$systemButtons .= '<input id="reboot_button" type="button" value="reboot system" onclick="sys_reboot();" class="btn btn-warning btn-block">';
-	$systemButtons .= '<input id="reset_button" type="button" value="reset settings" onclick="send_cmd_temporaer();setTimeout(function(){location.reload(true);}, 1000);" class="btn btn-danger btn-block">';
-	$systemButtons .= '</div>';
+	if (NAV_POS == "index") {
+		$systemButtons .= '<div>';
+		$systemButtons .= '<input id="toggle_stream" type="button" class="btn btn-default btn-block" value="' . $streamButton . '" onclick="set_stream_mode(this.value);">';
+		$systemButtons .= '<input id="shutdown_button" type="button" value="shutdown system" onclick="sys_shutdown();" class="btn btn-danger btn-block">';
+		$systemButtons .= '<input id="reboot_button" type="button" value="reboot system" onclick="sys_reboot();" class="btn btn-warning btn-block">';
+		$systemButtons .= '<input id="reset_button" type="button" value="reset settings" onclick="send_cmd_temporaer();setTimeout(function(){location.reload(true);}, 1000);" class="btn btn-danger btn-block">';
+		$systemButtons .= '</div>';
+	}
 ?>
 <script>
 function showSystemControllerButtons() {
